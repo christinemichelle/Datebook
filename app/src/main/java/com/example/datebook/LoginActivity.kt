c872a3b325d1c620373b9727306922ab87bc9340
Christinemichelle <christinekito39@gmail.com>
2023-09-26 15:38:51 +0300

    Datebook

--- app/src/main/java/com/example/datebook/LoginActivity.kt
+++ app/src/main/java/com/example/datebook/LoginActivity.kt
@@ -1,11 +1,71 @@
 package com.example.datebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.datebook.databinding.ActivityLoginBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseDatabase =FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
+        binding.LoginSignUp.setOnClickListener {
+            val LoginUsername = binding.LoginUsername.text.toString()
+            val LoginPassword = binding.LoginPassword.text.toString()
+
+            if (LoginUsername.isNotEmpty() && LoginPassword.isNotEmpty()){
+                LoginUser(LoginUsername, LoginPassword)
+            } else{
+                Toast.makeText(this@LoginActivity, "All fields are required", Toast.LENGTH_SHORT ).show()
+
+            }
+        }
+        binding.SignUpRedirect.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignupActivity::class.java))
+            finish()
+        }
     }
+
+    private  fun LoginUser(username:String, password: String){
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (datasnapshot.exists()){
+                    for (userSnapshot in datasnapshot.children){
+                        val userdata = userSnapshot.getValue(Userdata::class.java)
+
+                        if (userdata!= null && userdata.password == password){
+                            Toast.makeText(this@LoginActivity, "Login Successful" , Toast.LENGTH_LONG ).show()
+                            startActivity(Intent(this@LoginActivity, WelcomeActivity::class.java))
+                            finish()
+                            return
+
+                        }
+                    }
+                }
+                Toast.makeText(this@LoginActivity, "Login Failed" , Toast.LENGTH_LONG ).show()
+
+            }
+
+            override fun onCancelled(databaseerror: DatabaseError) {
+                Toast.makeText(this@LoginActivity,"Database error !:${databaseerror.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/example/datebook/SignupActivity.kt
+++ app/src/main/java/com/example/datebook/SignupActivity.kt
@@ -1,11 +1,67 @@
 package com.example.datebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.datebook.databinding.ActivitySignupBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivitySignupBinding
+    private lateinit var firebasedatabase: FirebaseDatabase
+    private lateinit var databasereference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebasedatabase =FirebaseDatabase.getInstance()
+        databasereference = firebasedatabase.reference.child("users")
+
+        binding.SignUp.setOnClickListener {
+            val signupUsername = binding.Username.text.toString()
+            val signupPassword = binding.Password.text.toString()
+
+            if (signupUsername.isNotEmpty() && signupPassword.isNotEmpty()){
+                signupUser(signupUsername, signupPassword)
+            } else{
+                Toast.makeText(this@SignupActivity, "All fields are required", Toast.LENGTH_SHORT ).show()
+
+            }
+
+        }
+        binding.LoginRedirect.setOnClickListener {
+            startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
+            finish()
+        }
+
     }
+    private fun signupUser(username: String, password: String ){
+        databasereference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (!datasnapshot.exists()){
+                    val id = databasereference.push().key
+                    val userdata = Userdata(id, username, password)
+                    databasereference.child(id!!).setValue(userdata)
+                    Toast.makeText(this@SignupActivity, "Signup Suuccessful", Toast.LENGTH_SHORT ).show()
+                    startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
+                    finish()
+                } else{
+                    Toast.makeText(this@SignupActivity,"User already exists", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@SignupActivity, "Error!: ${databaseError.message}", Toast.LENGTH_SHORT ).show()
+
+            }
+        })
+    }
+
 }
